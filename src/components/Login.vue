754eabda56d6021f664b98642d970b109140b461
weihancheng <2049874524@qq.com>
2019-12-23 17:32:01 +0800

    提交home初始页面

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -68,11 +68,15 @@ export default {
       this.loginForm.username = ''
       this.loginForm.password = ''
     },
+    // 点击登录按钮
     login () {
       this.$refs.loginFormRef.validate(async valid => {
         if (!valid) return
         const { data: res } = await this.$http.post('login', this.loginForm)
-        console.log(res)
+        if (res.data.status !== 200) return this.$message.error('登录失败!')
+        this.$message.success('登录成功!')
+        window.sessionStorage.setItem('token', res.data.token)
+        this.$router.push('/home')
       })
     }
   }
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
-import { Button, Form, FormItem, Input } from 'element-ui'
+import { Button, Form, FormItem, Input, Message, Alert } from 'element-ui'
 
 Vue.use(Button)
 Vue.use(Form)
 Vue.use(FormItem)
 Vue.use(Input)
+Vue.use(Message)
+Vue.use(Alert)
+Vue.prototype.$message = Message
+Vue.prototype.$alert = Alert
